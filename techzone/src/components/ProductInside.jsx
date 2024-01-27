63f8d8200a1b159fe43ecb23eefdb4936612b2c3
AshishSatyal <ashishsatyal4@gmail.com>
2024-01-27 23:25:46 +0545

    content in product page

--- techzone/src/components/ProductInside.jsx
+++ techzone/src/components/ProductInside.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { items } from "./AllData";
 
 const ProductInside = () => {
   return (
     <div className='container'>
       <div className='image-side'>
+        <img src={items[0].img} alt='chair' />
         <div className='other-image'></div>
       </div>
-      <div className='info-side'></div>
+      <div className='info-side'>
+        <p>
+          Sum is a series designed by the Danish designer Simon Legald, who is
+          the epitome of modern elegance. Elegant and resolute as the tip of a
+          wing, the armrests spread out invitingly, as if they want to embrace
+          you in the armchair's soft, homely space.
+        </p>
+      </div>
     </div>
   );
 };
--- techzone/src/pages/ProductPage.jsx
+++ techzone/src/pages/ProductPage.jsx
@@ -3,13 +3,14 @@ import Nav from "../components/Nav.jsx";
 import Footer from "../components/Footer.jsx";
 import Trending from "../components/Trending.jsx";
 import Newsletter from "../components/Newsletter.jsx";
+import ProductInside from "../components/ProductInside.jsx";
 
 const ProductPage = () => {
   return (
     <div>
       <Nav />
       <div className='product-info'>
-        <h1>This is product page</h1>
+        <ProductInside />
       </div>
       <Trending />
       <Newsletter />
